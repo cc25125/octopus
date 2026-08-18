e8fb611bfa7ad47d55d546bced073ff34fe1ab7d
DERIK CRUZ SILVA <u25125@academico.cotuca.unicamp.br>
2026-08-18 09:42:51 -0300

    Create Customer.java

---
+++ program/Customer.java
@@ -0,0 +1,33 @@
+package program;
+
+public class Customer {
+    private int id;
+    private String name;
+    private int discount;
+
+    public Customer(int id, String name, int discout) {
+        this.id = id;
+        this.name = name;
+        this.discount = discout;
+    }
+
+    public int getId() {
+        return id;
+    } 
+
+    public String getName() {
+        return name;
+    }
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + id + " " + discount;
+    }
+}
